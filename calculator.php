b7002be9ae4300b69edaff666f76596e9066ce5e
Rizal <rizalyunus@ukm.edu.my>
2015-10-26 10:25:20 +0800

    init calc class

---
+++ calculator.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Calculator{
+
+	function add(a,b)
+	{
+		return a + b;
+	}
+
+}
+
+/* End of file calculator.php */
+/* Location: .//C/Users/rizal/Desktop/gittot1/calculator.php */
